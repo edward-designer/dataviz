f107abce045402ccaeb25a1cb3caaadd3e22c8e5
gracedward <edward.chung.designer@gmail.com>
2023-12-01 12:21:38 +0000

    feat: add 3d octopus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,18 @@ import EnergyPriceCard from "./EnergyPriceCard";
 
 const Home = () => {
   return (
-    <div className="lg:col-[content] my-4 flex justify-start items-center">
-      <div className="max-w-full w-full md:w-[75%] min-w-[300px] z-10 bg-black/20 flex gap-2 p-4 flex-col lg:flex-row flex-wrap mb-20">
-        <EnergyPriceCard type="E" plan="agile" />
-        <EnergyPriceCard type="E" plan="tracker" />
-        <EnergyPriceCard type="G" plan="tracker" />
+    <div className="lg:col-[content] my-4 flex justify-start items-center relative z-0">
+      <div className="max-w-full w-full md:w-[75%] min-w-[300px] z-10 bg-black/20 mb-20">
+        <div className="pl-6 translate-y-3 text-sm font-extralight">
+          Cheapest Energy Price for {new Date().toLocaleDateString()}
+        </div>
+        <div className="flex gap-2 p-4 flex-col lg:flex-row flex-wrap">
+          <EnergyPriceCard type="E" plan="agile" />
+          <EnergyPriceCard type="E" plan="tracker" />
+          <EnergyPriceCard type="G" plan="tracker" />
+        </div>
       </div>
+      <Octopus3d />
     </div>
   );
 };
--- src/components/octopus/Octopus3d.tsx
+++ src/components/octopus/Octopus3d.tsx
@@ -4,7 +4,7 @@ import Spline from "@splinetool/react-spline";
 
 const Octopus3d = () => {
   return (
-    <div className="fixed inset-0">
+    <div className="fixed inset-0 z-0 top-16 lg:top-0">
       <Spline scene="https://prod.spline.design/iLMCaiZYV1bh6duh/scene.splinecode" />
     </div>
   );
