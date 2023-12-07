73e7f4afad5e031738a06f2e36beb94df6b1a5ce
gracedward <edward.chung.designer@gmail.com>
2023-12-07 11:57:12 +0000

    feat: add metadata

--- src/app/compare/page.tsx
+++ src/app/compare/page.tsx
@@ -1,5 +1,38 @@
 import UserApi from "@/components/octopus/UserApi";
-import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Compare Cheapest Tariffs | Octoprice",
+  description:
+    "Offers recommendations on the cheapest electricity and gas tariff based on your actual energy usage figures.",
+  keywords: [
+    "Octopus Energy",
+    "Compare Tariffs",
+    "Electricity",
+    "Gas",
+    "Cheapest",
+  ],
+  authors: [
+    { name: "Edward Chung", url: "https://edward-designer.github.io/" },
+  ],
+  metadataBase: new URL("https://octopriceuk.vercel.app/"),
+  openGraph: {
+    title: "Compare Cheapest Tariffs | Octoprice",
+    description:
+      "Offers recommendations on the cheapest electricity and gas tariff based on your actual energy usage figures.",
+    url: "https://octopriceuk.vercel.app/compare",
+    siteName: "Octoprice App",
+    images: [
+      {
+        url: "https://octopriceuk.vercel.app/octoprice-compare.jpg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_UK",
+    type: "website",
+  },
+};
 
 const Compare = () => {
   return (
