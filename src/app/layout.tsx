b96abd07c8cf355063083fcb509ce3b27961caf8
gracedward <edward.chung.designer@gmail.com>
2023-12-15 13:00:38 +0000

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { ReactNode } from "react";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react";
 
 import Providers from "./providers";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -118,6 +119,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <div className="min-h-screen p-4 lg:grid grid-rows-[100px_1fr_160px] lg:py-0 lg:p-0 lg:grid-cols-[[fullwidth_start]_minmax(0,5%)_[breakout_start]_minmax(0,5%)_[content_start]_minmax(1000px,_1fr)_[content_end]_minmax(0,5%)_[breakout_end]_minmax(0,5%)_[fullwidth_end]]">
               <Header className="lg:col-[content]" />
               {children}
+              <Analytics />
               <footer className="lg:col-[content] text-sm font-light text-white/60 mt-16">
                 &copy; {new Date().getFullYear()} Octoprice. Made with{" "}
                 {<IoMdHeart className="w-4 h-4 inline-block" />} by{" "}
