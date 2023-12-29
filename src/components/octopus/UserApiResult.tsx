87b6692ca70ad0cb769cd3ce97203fee88ea4d0d
gracedward <edward.chung.designer@gmail.com>
2023-12-29 15:42:17 +0000

    chores: amend notes to compare tariffs

--- src/components/octopus/UserApiResult.tsx
+++ src/components/octopus/UserApiResult.tsx
@@ -110,6 +110,10 @@ const UserApiResult = () => {
               The following analysis is based on your actual energy use pattern
               in the past year, showing which tariff suits you best!
               <Remark>
+                <em>
+                  [Notes: Currently Octopus Flux and Export tariffs are NOT
+                  supported.]
+                </em>{" "}
                 The figures presented here are an approximation of your annual
                 energy costs. Approximations and assumptions are used in the
                 calculations. The actual costs may vary a lot depending on the
