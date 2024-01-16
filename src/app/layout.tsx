d23b20440c08bcdd23a9771ee816b4777aa1ff93
gracedward <edward.chung.designer@gmail.com>
2024-01-16 12:25:28 +0000

    chores: update version number

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -132,7 +132,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                 Your Octopus info is stored on your browser only for retrieving
                 pricing and consumption data. <br />
                 <span className="text-accentBlue-500">
-                  <em>[ver 2024.1.3]</em> Bugs report & enquiries:{" "}
+                  <em>[ver 2024.1.16]</em> Bugs report & enquiries:{" "}
                   <a
                     href="mailto:edward.chung.dev@gmail.com"
                     className="underline"
