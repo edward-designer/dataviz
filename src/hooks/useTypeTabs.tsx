c5b0d8519997816a91195266e3aebfb3cdc45873
gracedward <edward.chung.designer@gmail.com>
2024-02-15 14:36:39 +0000

    style: tabs style

--- src/hooks/useTypeTabs.tsx
+++ src/hooks/useTypeTabs.tsx
@@ -13,7 +13,7 @@ const useTypeTabs = () => {
   const hasEExport = !!(value.EESerialNo && value.EMPAN);
   const hasGImport = !!(value.GSerialNo && value.MPAN);
 
-  const defaultType = hasEExport
+  const defaultType = hasEImport
     ? "E"
     : hasGImport
     ? "G"
@@ -24,46 +24,47 @@ const useTypeTabs = () => {
     if (defaultType) setCurrentType(defaultType);
   }, [defaultType]);
 
-  const Tabs = () => (
-    <div className="flex pl-6 z-30 sticky top-0 pt-4 bg-theme-950/70 backdrop-blur-md border-b border-accentPink-900">
-      {hasEImport && (
-        <Button
-          className={`border-t border-l border-r rounded-t-xl rounded-b-none ${
-            currentType === "E"
-              ? " border-accentPink-500 bg-accentPink-600 text-white"
-              : "bg-accentPink-950 text-accentPink-400 hover:bg-accentPink-500 hover:text-white border-accentPink-950/50"
-          }`}
-          clickHandler={() => setCurrentType("E")}
-        >
-          Electricity
-        </Button>
-      )}
-      {hasEExport && (
-        <Button
-          className={`border-t border-l border-r rounded-t-xl rounded-b-none ${
-            currentType === "EE"
-              ? " border-accentPink-500 bg-accentPink-600 text-white"
-              : "bg-accentPink-950 text-accentPink-400 hover:bg-accentPink-500 hover:text-white border-accentPink-950/50"
-          }`}
-          clickHandler={() => setCurrentType("EE")}
-        >
-          Electricity (Export)
-        </Button>
-      )}
-      {hasGImport && (
-        <Button
-          className={`border-t border-l border-r rounded-t-xl rounded-b-none ${
-            currentType === "G"
-              ? " border-accentPink-500 bg-accentPink-600 text-white"
-              : "bg-accentPink-950 text-accentPink-400 hover:bg-accentPink-500 hover:text-white border-accentPink-950/50"
-          }`}
-          clickHandler={() => setCurrentType("G")}
-        >
-          Gas
-        </Button>
-      )}
-    </div>
-  );
+  const Tabs = () =>
+    !(hasEImport && hasEExport && hasGImport) ? null : (
+      <div className="flex pl-6 z-30 sticky top-0 pt-4 bg-theme-950/70 backdrop-blur-md border-b border-accentPink-900 gap-1">
+        {hasEImport && (
+          <Button
+            className={`border-t border-l border-r rounded-t-xl rounded-b-none ${
+              currentType === "E"
+                ? " border-accentPink-500 bg-accentPink-600 text-white"
+                : "bg-accentPink-950 text-accentPink-400 hover:bg-accentPink-500 hover:text-white border-accentPink-950/50"
+            }`}
+            clickHandler={() => setCurrentType("E")}
+          >
+            Electricity
+          </Button>
+        )}
+        {hasEExport && (
+          <Button
+            className={`border-t border-l border-r rounded-t-xl rounded-b-none ${
+              currentType === "EE"
+                ? " border-accentPink-500 bg-accentPink-600 text-white"
+                : "bg-accentPink-950 text-accentPink-400 hover:bg-accentPink-500 hover:text-white border-accentPink-950/50"
+            }`}
+            clickHandler={() => setCurrentType("EE")}
+          >
+            Electricity (Export)
+          </Button>
+        )}
+        {hasGImport && (
+          <Button
+            className={`border-t border-l border-r rounded-t-xl rounded-b-none ${
+              currentType === "G"
+                ? " border-accentPink-500 bg-accentPink-600 text-white"
+                : "bg-accentPink-950 text-accentPink-400 hover:bg-accentPink-500 hover:text-white border-accentPink-950/50"
+            }`}
+            clickHandler={() => setCurrentType("G")}
+          >
+            Gas
+          </Button>
+        )}
+      </div>
+    );
   return { currentType, Tabs };
 };
 
